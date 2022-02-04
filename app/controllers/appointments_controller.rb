3bee739145440b7fa3b622655dbddb59f24a6dc1
David Watts <davidwatts@GS01169.geniesolutions.lan>
2022-02-04 16:31:06 +1000

    CRUD on appointments added

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,15 +1,36 @@
 class AppointmentsController < ApplicationController
+    rescue_from Exception do |e|
+        render json: { error: e }, status: 404
+    end 
+
     def index
         render json: Appointment.all
     end
 
     def create
-    
+        Appointment.create(entry_params)
     end
 
     def show
         render json: Appointment.find(params[:id])
     end
+
+    def update
+        appointmentToUpdate = Appointment.find(params[:id])
+        appointmentToUpdate.update(entry_params)
+    end
+
+    def destroy
+        appointmentToDestroy = Appointment.find(params[:id])
+        appointmentToDestroy.destroy()
+    end
+    # CCD2-AB5B
+    private
+
+    def entry_params
+        params.permit(:date, :provider_id, :patient)
+    end
    
 end
+
 
